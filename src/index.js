a74499cef1e951d19e4d56b74dace82a056005e6
LiudmilaTrafimuk <TrLuda@tut.by>
2017-09-18 23:01:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var result = [], f, s, transfer, resLine, l;
+    l = first.length + second.length;
+    for (let k=0; k<l; k++) {
+        result[k] = 0;
+    }    
+    for (let i=0; i<first.length; i++) {
+        transfer = 0;
+        for (let j=0; j<second.length; j++) {
+            
+            f = (i > 0) ? first.slice(-(i + 1), -i ) : first.slice(-1) ;
+            s = (j > 0) ? second.slice(-(j + 1), -j) : second.slice(-1);
+
+            result[i + j] += f * s + transfer;
+            transfer = (result[i + j] / 10) ^ 0;
+            result[i + j] = result[i + j] % 10;
+        }
+        result[i + second.length] += transfer;
+    }
+    if (result[l-1] === 0){
+        result.pop();
+    }
+    result.reverse();
+    resLine = result.join('');   
+    return resLine; 
 }
